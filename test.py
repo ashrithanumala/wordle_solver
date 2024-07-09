fdab8d4cddd33c66e06141ff12ee0d17664b2ff1
Ashrith Anumala <ashrithanumala04@gmail.com>
2024-07-08 22:22:44 -0500

    40% is testing

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import random
 import torch
+import sys
 from model import DQN
 from wordle_env import WordleEnv
-import sys
 
 def test_dqn(env, dqn, word_list):
     total_reward = 0
@@ -14,29 +14,55 @@ def test_dqn(env, dqn, word_list):
         action = word_list.index(guessing_word)
         
         next_state, reward, done = env.step(action)
-        print(next_state, guessing_word, env.target_word)
         total_reward += reward
         state = next_state
         
         if done:
             break
     
-    print("Test Reward:", total_reward)
+    return total_reward
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 test.py TARGET_WORD")
+        print("Usage: python3 test.py TARGET_WORD or python3 test.py all")
         sys.exit(1)
 
-    target_word = sys.argv[1]
+    target_arg = sys.argv[1]
     with open('data/past_wordle_answers.txt') as f:
         past_answers = [line.strip() for line in f]
     
     with open('data/wordle_words.txt') as f:
         word_list = [line.strip() for line in f]
     
-    env = WordleEnv(target_word, word_list)
-    dqn = DQN(len(target_word), len(word_list))
-    dqn.load_state_dict(torch.load("dqn_wordle.pth"))
-    
-    test_dqn(env, dqn, word_list)
+    if target_arg == "all":
+        total_rewards = 0
+        rewards = []
+        sample_size = int(0.4 * len(past_answers))
+        sampled_answers = random.sample(past_answers, sample_size)
+
+        for target_word in sampled_answers:
+            env = WordleEnv(target_word, word_list)
+            dqn = DQN(len(target_word), len(word_list))
+            dqn.load_state_dict(torch.load("dqn_wordle.pth"))
+            reward = test_dqn(env, dqn, word_list)
+            rewards.append(reward)
+            total_rewards += reward
+        
+        average_reward = total_rewards / len(past_answers)
+        max_reward = max(rewards)
+        min_reward = min(rewards)
+        print(f"Average Test Reward: {average_reward}")
+        print(f"Max Test Reward: {max_reward}")
+        print(f"Min Test Reward: {min_reward}")
+        print(f"Total Test Rewards: {total_rewards}")
+    else:
+        target_word = target_arg
+        if target_word not in past_answers:
+            print(f"Target word {target_word} is not in past_wordle_answers.txt")
+            sys.exit(1)
+        
+        env = WordleEnv(target_word, word_list)
+        dqn = DQN(len(target_word), len(word_list))
+        dqn.load_state_dict(torch.load("dqn_wordle.pth"))
+        reward = test_dqn(env, dqn, word_list)
+        print(f"Test Reward for {target_word}: {reward}")
